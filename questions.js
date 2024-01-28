fdc02af4d88ba3907a8b3ce9c3b280e65110bb86
João Vitor Cascão <37716511+JvCasc@users.noreply.github.com>
2024-01-28 17:09:39 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,13 +2,13 @@ const allQuestions = {
     q1: {
         question: "De onde é essa imagem ?",
         img: "../Images/demonSlayer.png",
-        answer: "Demon Slayer"
+        answer: ["Demon Slayer", "Kimetsu no Yaiba"]
         }
     ,
     q2: {
         question: "Que filme é esse ?",
         img: "../Images/society.jpg",
-        answer: "Snow Society"
+        answer: ["Snow Society", "A Sociedade da Neve"]
         }
     ,
     q3: {
@@ -20,7 +20,7 @@ const allQuestions = {
     q4: {
         question: "De que país é essa bandeira ?",
         img: "../Images/guiana_francesa.jpg",
-        answer: "Guiana Francesa"
+        answer: ["Guiana Francesa", "French Guiana"]
         }
     ,
     q5: {
@@ -31,15 +31,214 @@ const allQuestions = {
     ,
     q6: {
         question: "De quem é esse álbum ?",
-        img: "../Images/TameImpala.jpg",
+        img: "../Images/TameImpala.jpeg",
         answer: "Tame Impala"
         }
     ,
     q7: {
-        question: "De onde é esse filme ?",
-        img: "../Images/society.jpg",
-        answer: "Society"
+        question: "Que filme é esse ?",
+        img: "../Images/1917.webp",
+        answer: "1917"
         }
+    ,
+    q8: {
+        question: "Que filme é esse ?",
+        img: "../Images/aQuietPlace.webp",
+        answer: ["A Quiet Place", "Um Lugar Silencioso"]
+        }
+    ,
+    q9: {
+        question: "Que filme é esse ?",
+        img: "../Images/arrival.jpg",
+        answer: ["Arrival", "A Chegada"]
+        }
+    ,
+    q10: {
+        question: "Que filme é esse ?",
+        img: "../Images/babyDriver.jpg",
+        answer: ["Baby Driver", "Em Ritmo de Fuga"]
+        }
+    ,
+    q11: {
+        question: "Que filme é esse ?",
+        img: "../Images/coraline.webp",
+        answer: "Coraline"
+        }
+    ,
+    q12: {
+        question: "Que filme é esse ?",
+        img: "../Images/drive.jpg",
+        answer: "Drive"
+        }
+    ,
+    q13: {
+        question: "Que filme é esse ?",
+        img: "../Images/dune.webp",
+        answer: ["Dune", "Duna"]
+        }
+    ,
+    q14: {
+        question: "Que filme é esse ?",
+        img: "../Images/eliteSquad.jpg",
+        answer: ["Elite Squad", "Tropa de Elite"]
+        }
+    ,
+    q15: {
+        question: "Que filme é esse ?",
+        img: "../Images/encantation.jfif",
+        answer: "Encantation"
+        }
+    ,
+    q16: {
+        question: "Que filme é esse ?",
+        img: "../Images/fantasticMrFox.jpg",
+        answer: ["Fantastic Mr. Fox", "O Fantástico Sr. Raposo"]
+        }
+    ,
+    q17: {
+        question: "Que filme é esse ?",
+        img: "../Images/fightclub.jpg",
+        answer: ["Fight Club", "Clube da Luta"]
+        }
+    ,
+    q18: {
+        question: "Que filme é esse ?",
+        img: "../Images/greenBook.webp",
+        answer: "Green Book"
+        }
+    ,
+    q19: {
+        question: "Que filme é esse ?",
+        img: "../Images/inglouriousBasterds.jpg",
+        answer: ["Inglourious Basterds", "Bastardos Inglórios"]
+        }
+    ,
+    q20: {
+        question: "Que filme é esse ?",
+        img: "../Images/joker.jpg",
+        answer: ["Joker", "Coringa"]
+        }
+    ,
+    q21: {
+        question: "Que filme é esse ?",
+        img: "../Images/knivesOut.webp",
+        answer: ["Knives Out", "Entre Facas e Segredos"]
+        }
+    ,
+    q22: {
+        question: "Que filme é esse ?",
+        img: "../Images/nobody.jpg",
+        answer: ["Nobody", "Anônimo"]
+        }
+    ,
+    q23: {
+        question: "Que filme é esse ?",
+        img: "../Images/nope.jpg",
+        answer: ["Nope", "Não! Não Olhe!", "Não Não Olhe!", "Não Olhe"]
+        }
+    ,
+    q24: {
+        question: "Que filme é esse ?",
+        img: "../Images/oppenheimer.webp",
+        answer: "Oppenheimer"
+        }
+    ,
+    q25: {
+        question: "Que filme é esse ?",
+        img: "../Images/pearl.jpeg",
+        answer: "Pearl"
+        }
+    ,
+    q26: {
+        question: "Que filme é esse ?",
+        img: "../Images/knivesOut.webp",
+        answer: "Knives Out"
+        }
+    ,
+    q27: {
+        question: "Que filme é esse ?",
+        img: "../Images/prey.webp",
+        answer: ["Prey", "O Predador"]
+        }
+    ,
+    q28: {
+        question: "Que filme é esse ?",
+        img: "../Images/pulpFiction.webp",
+        answer: "Pulp Fiction"
+        }
+    ,
+    q29: {
+        question: "Que filme é esse ?",
+        img: "../Images/pussInBoots.png",
+        answer: ["Puss In Boots", "Gato de Botas", "Gato de Botas 2", "Puss In Boots 2"]
+        }
+    ,
+    q30: {
+        question: "Que filme é esse ?",
+        img: "../Images/scottPilgrim.webp",
+        answer: ["Scott Pilgrim", "Scott Pilgrim Contra o Mundo", "Scott Pilgrim vs the World"]
+        }
+    ,
+    q31: {
+        question: "Que filme é esse ?",
+        img: "../Images/spiderManAcrossTheSpiderVerse.jpg",
+        answer: ["Spider Man Across The Spider Verse", "Homem Aranha Através do Aranhaverso", "Homem Aranha Atraves do Aranhaverso"]
+        }
+    ,
+    q32: {
+        question: "Que filme é esse ?",
+        img: "../Images/theCreator.jpg",
+        answer: ["The Creator", "Resistência"]
+        }
+    ,
+    q33: {
+        question: "Que filme é esse ?",
+        img: "../Images/theGrandHotelBudapest.webp",
+        answer: ["The Grand Hotel Budapest", "O Grande Hotel Budapeste"]
+        }
+    ,
+    q34: {
+        question: "Que filme é esse ?",
+        img: "../Images/theMenu.jpg",
+        answer: ["The Menu", "O Menu", "Menu"]
+        }
+    ,
+    q35: {
+        question: "Que filme é esse ?",
+        img: "../Images/thePlatform.webp",
+        answer: ["The Platform", "O Poço", "Poço"]
+        }
+    ,
+    q36: {
+        question: "Que filme é esse ?",
+        img: "../Images/theWhale.jpg",
+        answer: ["The Whale", "A Baleia", "Baleia"]
+        }
+    ,
+    q37: {
+        question: "Que filme é esse ?",
+        img: "../Images/up.jpg",
+        answer: ["Up", "Up Altas Aventuras"]
+        }
+    ,
+    q38: {
+        question: "Que filme é esse ?",
+        img: "../Images/theWitch.webp",
+        answer: ["The Witch", "A Bruxa"]
+        }
+    ,
+    q39: {
+        question: "Que filme é esse ?",
+        img: "../Images/us.webp",
+        answer: ["Us", "Nós", "Nos"]
+        }
+    ,
+    q40: {
+        question: "Que filme é esse ?",
+        img: "../Images/x.webp",
+        answer: ["X", "X A Marca da Maldição"]
+        }
+    ,
     }
     
     export {allQuestions};
